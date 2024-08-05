b4216ba3cd162fc07da98575c9c93f60068dedc6
tarun <dhiman.cssolutions@gmail.com>
2024-08-05 15:10:16 +0530

    setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DatingApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ApplicationUser>ApplicationUsers { get; set; }
+    }
+}
